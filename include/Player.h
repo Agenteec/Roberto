fea3b2ebff2a42f771f1e17dbfeefbdf81be4973
strukfit <strukfit0@gmail.com>
2023-07-23 17:19:32 +0200

    Player update

--- include/Player.h
+++ include/Player.h
@@ -1,5 +1,48 @@
 #pragma once
-class Player
+#include <Entity.h>
+
+struct FlagsWASD
 {
+	FlagsWASD() :
+		wPressed(false),
+		aPressed(false),
+		sPressed(false),
+		dPressed(false)
+	{
+
+	}
+
+	bool wPressed;
+	bool aPressed;
+	bool sPressed;
+	bool dPressed;
+};
+
+void move(const FlagsWASD& wasd, sf::Sprite& sprite, const float& deltaTime, const float& speed);
+
+class Player : public Entity
+{
+public:
+
+	Player() :
+		hp(100.f),
+		maxHp(100.f),
+		speed(950.f),
+		hitted(false)
+	{}
+
+	void setHp(const float& hp);
+	void setMaxHp(const float& maxHp);
+	void setSpeed(const float& speed);
+	void setHittedFlag(const bool& hitted);
+
+	float getHp();
+	float getMaxHp();
+	float getSpeed();
+	bool isHitted();
+
+private:
+	float hp, maxHp, speed;
+	bool hitted;
 };
 
--- src/Player.cpp
+++ src/Player.cpp
@@ -1,1 +1,53 @@
 #include "Player.h"
+
+void move(const FlagsWASD& wasd, sf::Sprite& sprite, const float& deltaTime, const float& speed)
+{
+	if (wasd.wPressed)
+		sprite.move(0, -1.f * deltaTime * speed);
+	if (wasd.sPressed)
+		sprite.move(0, 1.f * deltaTime * speed);
+	if (wasd.aPressed)
+		sprite.move(-1.f * deltaTime * speed, 0);
+	if (wasd.dPressed)
+		sprite.move(1.f * deltaTime * speed, 0);
+}
+
+void Player::setHp(const float& hp)
+{
+	this->hp = hp;
+}
+
+void Player::setMaxHp(const float& maxHp)
+{
+	this->maxHp = maxHp;
+}
+
+void Player::setSpeed(const float& speed)
+{
+	this->speed = speed;
+}
+
+void Player::setHittedFlag(const bool& hitted)
+{
+	this->hitted = hitted;
+}
+
+float Player::getHp()
+{
+	return hp;
+}
+
+float Player::getMaxHp()
+{
+	return maxHp;
+}
+
+float Player::getSpeed()
+{
+	return speed;
+}
+
+bool Player::isHitted()
+{
+	return hitted;
+}
