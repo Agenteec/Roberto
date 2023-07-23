f9d99a869c67be3acf0bae5ee67b81aa7a63b164
strukfit <strukfit0@gmail.com>
2023-07-23 15:39:27 +0200

    Entity update

--- include/Entity.h
+++ include/Entity.h
@@ -1,5 +1,27 @@
 #pragma once
-class Entity
+
+#include <SFML/Graphics.hpp>
+#include <box2d/box2d.h>
+#include "GlobalConsts.h"
+
+class Entity : public sf::Sprite
 {
+public:
+	b2BodyDef bodyDef;
+	b2Body* body;
+	b2Fixture* fixture;
+	b2PolygonShape bodyShape;
+
+	void initBody(b2World* world, sf::Vector2f pos);
+
+	void setBodyOvalShape();
+
+	void setBodyBoxShape();
+
+	void setBodyPolygonShape(const b2Vec2 *vertices);
+
+	void setBodyPosition(const sf::Vector2f &vec);
+
+	void setBodyPosition(const b2Vec2 &vec);
 };
 
--- src/Entity.cpp
+++ src/Entity.cpp
@@ -1,1 +1,19 @@
 #include "Entity.h"
+
+void Entity::initBody(b2World* world, const sf::Vector2f pos)
+{
+	bodyDef.type = b2_dynamicBody;
+	bodyDef.position.Set(pos.x / SCALE, pos.y / SCALE);
+	body = world->CreateBody(&bodyDef);
+}
+
+void Entity::setBodyPosition(const sf::Vector2f& vec)
+{
+	setPosition(vec);
+	body->SetTransform(b2Vec2(vec.x / SCALE, vec.y / SCALE), body->GetAngle());
+}
+
+void Entity::setBodyPosition(const b2Vec2& vec)
+{
+	body->SetTransform(vec, body->GetAngle());
+}
