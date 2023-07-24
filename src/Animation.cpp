6b9aea333e11914a23af975e08f4097c172aff86
s1lence3 <bonnie1337.228@gmail.com>
2023-07-24 18:58:08 +0300

    animation start

---
+++ src/Animation.cpp
@@ -0,0 +1,54 @@
+#include "Animation.h"
+
+void Animation::setTexture(sf::Texture* texture, int size)
+{
+	this->texture = texture;
+	this->size = size;
+}
+
+void Animation::setTileSize(sf::Vector2f tileSize)
+{
+	this->tileSize = tileSize;
+}
+
+void Animation::setPosition(sf::Vector2f position)
+{
+	this->position = position;
+}
+
+void Animation::setFrameChangeTime(float length)
+{
+	this->frameChangeTime = length;
+}
+
+void Animation::setSize(int size)
+{
+	this->size = size;
+}
+
+sf::Texture* Animation::getTexture()
+{
+	return texture;
+}
+
+sf::Vector2f Animation::getTileSize()
+{
+	return tileSize;
+}
+
+sf::Vector2f Animation::getPosition()
+{
+	return position;
+}
+
+float Animation::getFrameChangeTime()
+{
+	return frameChangeTime;
+}
+
+int Animation::getSize()
+{
+	return size;
+}
+
+void play()
